f3416572cfa1ea859b3f4ee93850afb779317c5d
Zojae031 <edgar3@naver.com>
2019-08-05 15:41:26 +0900

    [현호죽이기 part1] 게시판 글올리기

---
+++ death/app/src/main/java/jsp/death/Parser.kt
@@ -0,0 +1,50 @@
+package jsp.death
+
+import android.util.Log
+import org.jsoup.Connection
+import org.jsoup.Jsoup
+import org.jsoup.nodes.FormElement
+
+object Parser {
+    private val loginUrl = "http://116.126.97.126:12345/BBS/login.jsp"
+    private lateinit var session: Map<String, String>
+    private var count = 0
+    fun login() {
+        Thread {
+            val res = Jsoup.connect(loginUrl)
+                .method(Connection.Method.GET)
+                .execute()
+            session = res.cookies()
+
+            val form = res.parse().select("form").first() as FormElement
+            form.let {
+                it.select("[name=userID]").`val`("zoz")
+                it.select("[name=userPassword]").`val`("wodud")
+                it.submit()
+                    .cookies(session)
+                    .post()
+            }
+
+
+        }.start()
+    }
+
+    fun writePost() {
+        Thread {
+            val write = Jsoup.connect("http://116.126.97.126:12345/BBS/write.jsp")
+                .cookies(session)
+                .method(Connection.Method.POST)
+                .execute()
+            val writeForm = write.parse().select("form").first() as FormElement
+            Log.e("form", writeForm.html())
+            val a = writeForm.let {
+                it.select("[name=bbsTitle]").`val`("앙기모찌$count")
+                it.select("[name=bbsContent]").`val`("asd$count")
+                it.submit()
+                    .cookies(session)
+                    .post()
+            }
+            count++
+        }.start()
+    }
+}
